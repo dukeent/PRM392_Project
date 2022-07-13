4f323a7986fa24911a68b8951ba1f7bf8f60b898
toanvocuc <86276817+toanvocuc@users.noreply.github.com>
2022-07-13 22:31:23 +0700

    view

--- app/src/main/java/com/fptu/android/userinterface/MainActivity.java
+++ app/src/main/java/com/fptu/android/userinterface/MainActivity.java
@@ -4,6 +4,7 @@ package com.fptu.android.userinterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
