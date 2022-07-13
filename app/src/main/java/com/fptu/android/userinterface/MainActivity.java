d5cf05ed8e8079d673e5cad4d7788feee48d8b87
BuiHai1502 <92239360+BuiHai1502@users.noreply.github.com>
2022-07-13 14:36:17 +0700

    wheel

--- app/src/main/java/com/fptu/android/userinterface/MainActivity.java
+++ app/src/main/java/com/fptu/android/userinterface/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream
         logOut = findViewById(R.id.btnlogout);
 
         logOut.setOnClickListener(new View.OnClickListener() {
@@ -27,5 +28,52 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "logout success", Toast.LENGTH_LONG).show();
             }
         });
+=======
+//        logOut = findViewById(R.id.btnlogout);
+        bottomNavigationView = findViewById(R.id.bottomNavigation) ;
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        // secssion
+//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        if (user != null) {
+//            // User is signed in
+                // nếu user đã login thì cho phép mua hàng
+//        } else {
+//            // No user is signed in
+            // nếu user chưa login thì chuyển về trang login
+//        }
+//        logOut.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, Login.class));
+//
+//            }
+//        });
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        switch (id){
+            case R.id.item5:
+                startActivity(new Intent(this, UserProfile.class));
+                return true ;
+            case R.id.item4:
+                startActivity(new Intent(this, WheelSpin.class));
+                return true ;
+            case R.id.item3:
+                startActivity(new Intent(this, ViewStaff.class));
+                return true ;
+            case R.id.item2:
+                startActivity(new Intent(this, ViewProduct.class));
+                return true ;
+            case R.id.item1:
+                Toast.makeText(MainActivity.this, "1", Toast.LENGTH_LONG).show();
+                return true ;
+        }
+
+
+        return false;
+>>>>>>> Stashed changes
     }
 }
